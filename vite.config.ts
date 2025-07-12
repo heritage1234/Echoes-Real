6dd9e906b11ad3b6cd5776a523703c8acbed7709
ayomide-3380 <alaoayomide700@gmail.com>
2025-07-12 22:37:20 +0100

    Fix: adding Netlify dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    target: "esnext", // ensures ES module output
+    outDir: "dist",    // confirm dist output
+    assetsDir: "assets",
+  },
+});
